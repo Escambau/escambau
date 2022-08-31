df8d92f397926364aeffc6bd787f9745583eb25c
Carlos Mesquita <40701078+shirator@users.noreply.github.com>
2022-08-31 18:32:39 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
 import "./App.css";
-import ConfirmTradeModal from "./components/ConfirmTradeModal";
-import HeaderUnlogged from "./components/HeaderUnlogged";
 import GlobalStyle from "./styles/global";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <ConfirmTradeModal />
-      <HeaderUnlogged />
     </div>
   );
 }
