78668f863d421d0a8cec4811d3968288e40bd007
Ingleson <jovemtksstranger@gmail.com>
2022-09-02 09:47:22 -0300

    ajustes para pull

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
-import { AddProduct } from "./components/AddProduct";
 import GlobalStyle from "./styles/global";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AddProduct/>
     </div>
   );
 }
