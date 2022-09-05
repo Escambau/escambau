b336616977e13504a5d9f9b39bdedd7f000c2432
Ingleson <jovemtksstranger@gmail.com>
2022-09-05 12:30:55 -0300

    pasta de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import "./App.css";
-import React, { useContext } from "react";
 import GlobalStyle from "./styles/global";
-import MoreInfo from "./components/MoreInfo";
 import { ToastContainer } from "react-toastify";
-import { Routes, Route, Link } from "react-router-dom";
-import { AddProduct } from "./pages/AddProduct";
+import { EscambauRoutes } from "./routes";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <ToastContainer />
-      <Routes>
-        <Route path="/moreinfo" element={<MoreInfo />} />
-        <Route path="/addproduct" element={<AddProduct/>} />
-      </Routes>
+      <EscambauRoutes />
     </div>
   );
 }
