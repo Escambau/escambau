38c464513177bf6728d4aa9466270b43d8c127bf
AllanSalatino <98770586+AllanSalatino@users.noreply.github.com>
2022-09-05 14:40:27 -0300

    cancela

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,34 @@ import { ToastContainer } from "react-toastify";
 import {LoginModal} from "./components/LoginModal";
 import { Routes, Route, Link } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+<<<<<<< Updated upstream
 import User from "./pages/User";
 import Register from "./pages/Register";
 import EditProduct from "./pages/EditProduct";
 import { AddProduct } from "./components/AddProduct";
+=======
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <ToastContainer />
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
       <Routes>
         <Route path="/" element={<Dashboard />}/>
         <Route path="/profile" element={<User />} />
         <Route path="/register" element={<Register />} />
         <Route path="/edit-product" element={<EditProduct />} />
         <Route path="/moreinfo" element={<MoreInfo />} />
+<<<<<<< Updated upstream
         <Route path="/add-product" element={<AddProduct />}/>
+=======
+        <Route path="/" element={<Dashboard />} />
+>>>>>>> Stashed changes
       </Routes>
     </div>
   );
