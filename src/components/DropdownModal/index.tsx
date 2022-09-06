0a5dbd31b7d00c31600dc849623bc70a2625f58d
AllanSalatino <98770586+AllanSalatino@users.noreply.github.com>
2022-09-06 16:03:02 -0300

    Modal dropdown com animação

--- src/components/DropdownModal/index.tsx
+++ src/components/DropdownModal/index.tsx
@@ -1,29 +1,46 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../contexts/UserContext";
-import { Container } from "./style";
+import { Container, Overlay } from "./style";
+import { motion } from "framer-motion";
 
 const DropdownModal = () => {
-  const { logOut, isDropdownModal, setIsDropdownModal } = useContext(UserContext);
+  const { logOut, isDropdownModal, setIsDropdownModal } =
+    useContext(UserContext);
   const navigate = useNavigate();
 
   return (
     <>
       {isDropdownModal ? (
-        <Container>
-          <button className="profile" onClick={() => {
-            navigate("/profile")
-            setIsDropdownModal(false)
-            }}>
-            Perfil
-          </button>
-          <button className="exit" onClick={() => {
-            logOut()
-            setIsDropdownModal(false)
-            }}>
-            Sair
-          </button>
-        </Container>
+        <Overlay onClick={() => setIsDropdownModal(false)}>
+          <motion.div
+            initial={{ opacity: 0, x: 0, y: -500 }}
+            animate={{ opacity: 1, x: -70, y: -370 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Container>
+              <button
+                className="profile"
+                onClick={() => {
+                  navigate("/profile");
+                  setIsDropdownModal(false);
+                }}
+              >
+                Perfil
+              </button>
+              <button
+                className="exit"
+                onClick={() => {
+                  logOut();
+                  setIsDropdownModal(false);
+                }}
+              >
+                Sair
+              </button>
+            </Container>
+          </motion.div>
+        </Overlay>
       ) : (
         <></>
       )}
